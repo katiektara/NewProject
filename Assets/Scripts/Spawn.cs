7e543c54696126388d764b16e78c0d153d67d473
katiektara <mskater@bk.ru>
2017-12-18 17:26:35 +0300

    //v2.1

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public Transform[] spawnLocation;
+    public GameObject[] whatToSpawnPrefab;
+    public GameObject[] whatToSpawnClone;
+
+    void spawnSomethingAwesome()
+    {
+        whatToSpawnClone[0] = Instantiate(whatToSpawnPrefab[0], spawnLocation[0].transform.position, Quaternion.Euler(0, 0, 0)) as GameObject;
+    }
+}
